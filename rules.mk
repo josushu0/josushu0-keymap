db34753f138284f973e08c2cb31d9e23b46fa96d
Josué Martell <josue.martell17@gmail.com>
2023-05-10 20:47:14 -0600

    Initial commit

---
+++ rules.mk
@@ -0,0 +1,5 @@
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+OLED_ENABLE= yes            # OLED display
+WPM_ENABLE= yes
+LTO_ENABLE = yes			# Makes hex file smaller
